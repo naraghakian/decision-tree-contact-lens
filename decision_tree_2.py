f625a74b6de5fa5d905ab2823b9829dc13cdf210
Nareh Aghakian <narehaghakian@Narehs-MacBook-Pro.local>
2026-03-07 18:34:26 -0800

    finished decision tree assignment

--- decision_tree_2.py
+++ decision_tree_2.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Nareh Aghakian
+# FILENAME: decision_tree_2.py
+# SPECIFICATION: The program trains decision tree models using tree contact lens training datasets and tests them on a test dataset calculating the average accuracy in 10 runs.
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 8 hours
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: YOU ARE ALLOWED TO USE ANY PYTHON LIBRARY TO COMPLETE THIS PROGRAM
@@ -28,15 +28,46 @@ for ds in dataSets:
 
     #Reading the training data in a csv file using pandas
     # --> add your Python code here
-    dbTraining =
+    df_training = pd.read_csv(ds)
+    for _, row in df_training.iterrows():
+        dbTraining.append(row.tolist())
 
     #Transform the original categorical training features to numbers and add to the 4D array X.
     #For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
     #--> add your Python code here
+    for data in dbTraining:
+        age = data[0]
+        spectacle = data[1]
+        astigmatism = data[2]
+        tear = data[3]
+
+        if age == 'Young':
+            age = 1
+        elif age == 'Prepresbyopic':
+            age =2
+        else:
+            age =3
+        if spectacle == 'Myope':
+            spectacle = 1
+        else:
+            spectacle = 2
+        if astigmatism == 'No':
+            astigmatism = 1
+        else:
+            astigmatism = 2
+        if tear == 'Reduced':
+            tear = 1
+        else: 
+            tear =2
+        X.append([age, spectacle, astigmatism, tear])
 
     #Transform the original categorical training classes to numbers and add to the vector Y.
     #For instance Yes = 1 and No = 2, Y = [1, 1, 2, 2, ...]
     #--> add your Python code here
+        if data[4] == 'Yes':
+             Y.append(1)
+        else:
+            Y.append(2)
 
     #Loop your training and test tasks 10 times here
     for i in range (10):
@@ -44,25 +75,58 @@ for ds in dataSets:
        # fitting the decision tree to the data using entropy as your impurity measure and maximum depth = 5
        # --> addd your Python code here
        # clf =
-
+       clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=5)
+       clf = clf.fit(X,Y)
        #Read the test data and add this data to dbTest
        #--> add your Python code here
-
+       correct = 0
+       total = 0
        for data in dbTest:
            #Transform the features of the test instances to numbers following the same strategy done during training,
            #and then use the decision tree to make the class prediction. For instance: class_predicted = clf.predict([[3, 1, 2, 1]])[0]
            #where [0] is used to get an integer as the predicted class label so that you can compare it with the true label
            #--> add your Python code here
+        age = data[0]
+        spectacle = data[1]
+        astigmatism = data[2]
+        tear = data[3]
 
+        if age == 'Young':
+            age = 1
+        elif age == 'Prepresbyopic':
+            age = 2
+        else:
+            age = 3
+        if spectacle == 'Myope':
+            spectacle = 1
+        else:
+            spectacle = 2
+        if astigmatism == 'No':
+            astigmatism = 1
+        else:
+            astigmatism = 2
+        if tear == 'Reduced':
+            tear = 1
+        else:
+            tear = 2
+        class_predicted = clf.predict([[age, spectacle, astigmatism, tear]])[0]
+         
+        
            #Compare the prediction with the true label (located at data[4]) of the test instance to start calculating the accuracy.
            #--> add your Python code here
+        true_label = 1 if data[4] == 'Yes' else 2
+        if class_predicted == true_label:
+            correct += 1
+        total += 1
 
     #Find the average of this model during the 10 runs (training and test set)
     #--> add your Python code here
+        accuracy = correct / total
 
     #Print the average accuracy of this model during the 10 runs (training and test set).
     #Your output should be something like that: final accuracy when training on contact_lens_training_1.csv: 0.2
     #--> add your Python code here
+        print("final accuracy when training on", ds, ":", accuracy)
 
 
 
